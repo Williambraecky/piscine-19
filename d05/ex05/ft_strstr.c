62112759d52ccbb4d1da5f31823d92750aeca920
Williambraecky <williambraeckman@yahoo.fr>
2018-06-07 21:08:04 +0200

    fix ft_strstr

--- d05/ex05/ft_strstr.c
+++ d05/ex05/ft_strstr.c
@@ -10,40 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (*(str + i))
-		i++;
-	return (i);
-}
-
 char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int j;
-	int k;
-	int l;
-
-	if (to_find[0] == '\0')
-		return (str);
-	i = ft_strlen(str);
-	j = ft_strlen(to_find);
-	if (j > i)
-		return (0);
-	k = -1;
-	while (++k < i - j + 1)
+	
+	i = 0;
+	while (str[i] != '\0' && (j = 0) == 0)
 	{
-		if (str[k] == to_find[0])
+		while (str[i + j] == to_find[j])
 		{
-			l = -1;
-			while (++l < j)
-				if (str[k + l] != to_find[l])
-					break ;
-			return (str + k);
+			if (to_find[j + 1] == '\0')
+				return (str + i);
+			j++;
 		}
+		i++;
 	}
 	return (0);
 }
